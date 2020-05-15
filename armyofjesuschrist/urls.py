23ce0a7ac8ec114647c48c02e4e403a4b3ab981f
mikeangelo6996 <57066432+mikeangelo6996@users.noreply.github.com>
2020-05-15 13:32:59 +0100

    initial

---
+++ armyofjesuschrist/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+# app_name ='app_name'
+urlpatterns=[
+    path("",views.index ,name='home'),
+    path("sermon/", views.sermonlist.as_view(), name='sermon'),
+    path("Ministry/",views.Ministry ,name='department'),
+    path("video/", views.bloghome.as_view(), name='video'),
+    path('<int:year>/<int:month>/',views.bloghome.as_view(month_format='%m'), name="archive_month_numeric"),
+    path("detail/<int:pk>/", views.detailpost.as_view(), name='detail'),
+    path('contact/',views.contact, name='contact'),
+    path("videodetail/<int:pk>/", views.videodetail.as_view(), name='detailvideo'),
+    path("donation/", views.donationpage, name='donation'),
+    path('payment/',views.payment, name='payment'),
+]
